66a966f06a89ef58ed56a496d3f639ae589398b0
ink0rr <tomflynnbeltsazar@gmail.com>
2025-01-26 17:41:22 +0700

    Merge core

--- core/core.go
+++ core/core.go
@@ -2,6 +2,11 @@ package core
 
 import "github.com/ink0rr/rockide/internal/protocol"
 
+type Project struct {
+	BP string
+	RP string
+}
+
 type Reference struct {
 	Value string
 	URI   protocol.DocumentURI
--- core/project.go
+++
@@ -1,6 +0,0 @@
-package core
-
-type Project struct {
-	BP string
-	RP string
-}
