aab79bfc3996562019d20647181e6741a663847b
ink0rr <tomflynnbeltsazar@gmail.com>
2025-05-01 18:08:39 +0700

    Handle loot table type

--- handlers/loot_table.go
+++ handlers/loot_table.go
@@ -4,6 +4,7 @@ import (
 	"slices"
 
 	"github.com/ink0rr/rockide/core"
+	"github.com/ink0rr/rockide/internal/jsonc"
 	"github.com/ink0rr/rockide/shared"
 	"github.com/ink0rr/rockide/stores"
 	"github.com/ink0rr/rockide/vanilla"
@@ -11,7 +12,12 @@ import (
 
 var LootTable = newJsonHandler(shared.LootTableGlob, []jsonHandlerEntry{
 	{
-		Path:    []shared.JsonPath{shared.JsonValue("**/entries/*/name")},
+		Path: []shared.JsonPath{shared.JsonValue("**/entries/*/name")},
+		Matcher: func(params *jsonParams) bool {
+			parent := params.getParentNode()
+			entryType := jsonc.FindNodeAtLocation(parent, jsonc.Path{"type"})
+			return entryType != nil && entryType.Value == "item"
+		},
 		Actions: completions | definitions | rename,
 		Source: func(params *jsonParams) []core.Reference {
 			return stores.Item.Get("id")
@@ -20,5 +26,21 @@ var LootTable = newJsonHandler(shared.LootTableGlob, []jsonHandlerEntry{
 			return slices.Concat(stores.Attachable.Get("id"), stores.Entity.Get("item_id"), stores.Item.Get("item_id"), stores.LootTable.Get("item_id"), stores.Recipe.Get("item_id"))
 		},
 		VanillaData: vanilla.ItemIdentifiers,
+	},
+	{
+		Path: []shared.JsonPath{shared.JsonValue("**/entries/*/name")},
+		Matcher: func(params *jsonParams) bool {
+			parent := params.getParentNode()
+			entryType := jsonc.FindNodeAtLocation(parent, jsonc.Path{"type"})
+			return entryType != nil && entryType.Value == "loot_table"
+		},
+		Actions: completions | definitions,
+		Source: func(params *jsonParams) []core.Reference {
+			return stores.LootTable.Get("path")
+		},
+		References: func(params *jsonParams) []core.Reference {
+			return nil
+		},
+		VanillaData: vanilla.LootTablePaths,
 	},
 })
--- stores/loot_table.go
+++ stores/loot_table.go
@@ -1,6 +1,9 @@
 package stores
 
-import "github.com/ink0rr/rockide/shared"
+import (
+	"github.com/ink0rr/rockide/internal/jsonc"
+	"github.com/ink0rr/rockide/shared"
+)
 
 var LootTable = &JsonStore{
 	pattern:  shared.LootTableGlob,
@@ -9,6 +12,18 @@ var LootTable = &JsonStore{
 		{
 			Id:   "item_id",
 			Path: []shared.JsonPath{shared.JsonValue("**/entries/*/name")},
+			Transform: func(node *jsonc.Node) *string {
+				nodeValue, ok := node.Value.(string)
+				if !ok || node.Parent == nil {
+					return nil
+				}
+				parent := node.Parent.Parent
+				entryType := jsonc.FindNodeAtLocation(parent, jsonc.Path{"type"})
+				if entryType != nil && entryType.Value == "item" {
+					return &nodeValue
+				}
+				return nil
+			},
 		},
 	},
 }
