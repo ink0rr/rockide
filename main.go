c5d9c11accb45f76fc8fb0ea66c603b346d992c0
ink0rr <tomflynnbeltsazar@gmail.com>
2025-11-01 22:25:40 +0700

    Add version flag

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 
 	"github.com/ink0rr/rockide/server"
 	"github.com/sourcegraph/jsonrpc2"
 )
 
+var version = "dev"
+
 func main() {
+	printVersion := flag.Bool("version", false, "Print version")
+	flag.Parse()
+	if *printVersion {
+		fmt.Println(version)
+		return
+	}
+
 	ctx := context.Background()
 	handler := jsonrpc2.HandlerWithError(server.Handle)
 	stream := jsonrpc2.NewBufferedStream(&stdio{}, jsonrpc2.VSCodeObjectCodec{})
